e60ca64163a1b61c489805ddb3b331a8bc4809a7
LovreCikojevic120 <56341053+LovreCikojevic120@users.noreply.github.com>
2021-11-10 17:46:24 +0100

    Potez 2

--- TicTacToe3/Program.cs
+++ TicTacToe3/Program.cs
@@ -9,7 +9,7 @@ namespace TicTacToe3
             Console.WriteLine("Lovre ima x pobjeda i y poraza!");
             Console.WriteLine("Bartol ima z pobjeda i n poraza!");
 
-            Console.WriteLine(" x |   |   ");
+            Console.WriteLine(" x |   | x ");
             Console.WriteLine("   | 0 |   ");
             Console.WriteLine("   |   |   ");
         }

8f25814ecc626d5003e13d5d347a269f09967f21
Bartol Deak <b@bdeak.net>
2021-11-10 16:37:04 +0100

    setup

---
+++ TicTacToe3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TicTacToe3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lovre ima x pobjeda i y poraza!");
+            Console.WriteLine("Bartol ima z pobjeda i n poraza!");
+
+            Console.WriteLine("   |   |   ");
+            Console.WriteLine("   |   |   ");
+            Console.WriteLine("   |   |   ");
+        }
+    }
+}
